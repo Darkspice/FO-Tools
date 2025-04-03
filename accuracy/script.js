1d07df49a827d3e8ca967d01836a7c7898dbd341
darkspice <darkspice@yandex.ru>
2025-04-03 22:23:03 +0400

    feat: new session updates

--- accuracy/script.js
+++ accuracy/script.js
@@ -27,7 +27,7 @@ function calculate() {
   const perceptionf = +perception.value;
 
   const lightf = light.checked ? 10 - rangef / 6 : 0;
-  const implantf = implant.checked ? 3 : 0;
+  const implantf = implant.checked ? 25 : 0;
   const ncrf = ncr.checked ? 25 : 0;
 
   const fastshooterf = fastshooter.checked ? -40 : 0;
